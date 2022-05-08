820f90c86cce342002590d5eb2d9d176d59cf7e7
Ammar <ammarferchichi7@gmail.com>
2022-05-08 14:49:59 +0100

    Side Drawer animation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
@@ -8,34 +8,34 @@ import { Fragment } from "react/cjs/react.production.min";
 import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = (props) => {
-  const [drawerIsOpen,setDrawerIsOpen] = useState(false);
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
   const OpenDrawer = () => {
     setDrawerIsOpen(true);
-  }
+  };
   const CloseDrawer = () => {
     setDrawerIsOpen(false);
-  }
+  };
   return (
     <Fragment>
-    {drawerIsOpen && <Backdrop onClick={CloseDrawer}/>}
-    {drawerIsOpen && <SideDrawer>
-      <nav className="main-navigation__drawer-nav">
-        <NavLinks/>
-      </nav>
-    </SideDrawer>}
-    <MainHeader>
-      <button className="main-navigation__menu-btn" onClick={OpenDrawer}>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Yourplaces</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-          <NavLinks/>
-      </nav>
-    </MainHeader>
+      {drawerIsOpen && <Backdrop onClick={CloseDrawer} />}
+      <SideDrawer show={drawerIsOpen}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={OpenDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Yourplaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
     </Fragment>
   );
 };
